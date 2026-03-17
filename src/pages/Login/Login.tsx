fbf832779a745cdcd2f42b3feee23be84bf0a6be
nikogniko <gorbach.nika.m@gmail.com>
2026-03-17 17:50:58 +0200

    update manager and driver cabinet UI, create PageHeader

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,24 @@
 import LoginForm from "../../shared/ui/loginForm/LoginForm";
 import styles from "./Login.module.scss";
+import PageHeader from "../../shared/ui/PageHeader/PageHeader";
+import textData from "../../textData/ua.json";
 
 const Login = () => {
   return (
     <div className={styles["login"]}>
-      <div className={styles["login__title"]}>
+      {/* <div className={styles["login__title"]}>
         <h1>
           БУДЬ ЛАСКА, ОБЕРІТЬ СВОЮ <span>ПРОФЕСІЮ</span>
         </h1>
+      </div> */}
+      <div>
+        <PageHeader
+          textStart={textData.homePage.authPromo.textStart}
+          textAccent={textData.homePage.authPromo.textAccent}
+          textEnd={textData.homePage.authPromo.textEnd}
+        />
       </div>
+
       <div style={{ marginTop: 50, marginBottom: 50 }}>
         <LoginForm />
       </div>
